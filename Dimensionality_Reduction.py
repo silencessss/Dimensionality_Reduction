a8d7d2966c27a96c6590616a21327cac61f0ba4a
ChanHT <chanhts323@gmail.com>
2022-06-08 18:37:29 +0800

    Add files via upload

--- Dimensionality_Reduction.py
+++ Dimensionality_Reduction.py
@@ -1,6 +1,7 @@
 """
 Created on Thu Jun 08 04:04:29 2022
 @author: PeterChan
+#reference: https://towardsdatascience.com/visualising-high-dimensional-datasets-using-pca-and-t-sne-in-python-8ef87e7915b
 """
 from __future__ import print_function
 from operator import ge
@@ -8,11 +9,13 @@ import time
 import numpy as np
 import pandas as pd
 import keras
+from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
+#import DataLoader
 
 class config:
     TARGET_H = 28
@@ -26,6 +29,19 @@ class load_data:
         x_train = x_train.astype('float32')/255.0
         x_train = x_train.reshape(60000,784)
         return x_train,y_train
+    def get_iris():
+        iris = load_iris()
+        x = iris.data
+        y = iris.target
+        return x,y
+    def get_AcneECK():
+        train_x,train_y,test_x,test_y,labelsarr = DataLoader.AcneECK.byDir()
+        train_x = train_x.astype('float32')/255.0
+        train_x = train_x.reshape(460,50176)
+        train_y = np.array(train_y)
+        print('labelsarr',labelsarr.shape)
+        
+        return train_x,labelsarr
 class visualization:
     def input_data(feat_cols):
         #plt.gray()
@@ -60,7 +76,7 @@ class dimensionality:
 
         df_subset['tsne-2d-one'] = tsne_results[:,0]
         df_subset['tsne-2d-two'] = tsne_results[:,1]
-        plt.figure(figsize=(16,10))
+        plt.figure()#figsize=(16,10)
         sns.scatterplot(
             x="tsne-2d-one", y="tsne-2d-two",
             hue="y",
@@ -68,7 +84,7 @@ class dimensionality:
             data=df_subset,
             legend="full",
             alpha=0.3
-        )
+        ).set(title='T-SNE projection')
         plt.show()
 
     def PCA_dimensionality(df,feat_cols,rndperm):
@@ -80,7 +96,7 @@ class dimensionality:
         print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
 
         # 二維可視
-        plt.figure(figsize=(16,10))
+        plt.figure()
         sns.scatterplot(
             x="pca-one", y="pca-two",
             hue="y",
@@ -88,15 +104,15 @@ class dimensionality:
             data=df.loc[rndperm,:],
             legend="full",
             alpha=0.3
-        )
+        ).set(title='PCA-2d projection')
 
         # 三維可視
-        ax = plt.figure(figsize=(16,10)).gca(projection='3d')
+        ax = plt.figure().gca(projection='3d')
         ax.scatter(
             xs=df.loc[rndperm,:]["pca-one"], 
             ys=df.loc[rndperm,:]["pca-two"], 
             zs=df.loc[rndperm,:]["pca-three"], 
-            c=df.loc[rndperm,:]["y"], 
+            #c=df.loc[rndperm,:]["y"], 
             cmap='tab10'
         )
         ax.set_xlabel('pca-one')
@@ -110,6 +126,6 @@ class dimensionality:
 X,y = load_data.get_mnist()
 df,feat_cols = transform(X,y)
 rndperm = randomly(df)
-visualization.input_data(feat_cols)
+#visualization.input_data(feat_cols)
 dimensionality.PCA_dimensionality(df,feat_cols,rndperm)
 dimensionality.tSNE_dimensionality(df,feat_cols,rndperm)
